e80a9403161a3cee8c3b4b87c734791738185490
Lazlo Mita <lazlo.mita@avantica.net>
2019-02-26 14:54:09 -0400

    Adding some code to the Abstract Demo

--- JavaAbstractClassDemo/src/com/avantica/demo/AbstractDemo.java
+++ JavaAbstractClassDemo/src/com/avantica/demo/AbstractDemo.java
@@ -4,12 +4,17 @@ package com.avantica.demo;
 public class AbstractDemo {
 	
 	public static void main(String [] args) {
+		
 		/* Following is not allowed and would raise error */
 		//Employee e = new Employee("George W.", "Houston, TX", 43);
 		
-		Employee e = new Salary("George W.", "Houston, TX", 43, 1200);
+		Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+		Employee e = new Salary("John Adams", "Boston, MA", 2, 2400.00);
 		
+		System.out.println("Call mailCheck using Salary reference --");
+		s.mailCheck();
 		System.out.println("\n Call mailCheck using Employee reference--");
-		e.mailCheck();
+		e.mailCheck();		
 	}
 }
+
